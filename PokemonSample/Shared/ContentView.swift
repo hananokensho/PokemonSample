7d1c3428a794e342c7794e1d66bb0f22f8e3086f
kensho hanano <>
2022-12-09 23:26:27 +0900

    プロジェクト作成

---
+++ PokemonSample/Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by kensho hanano on 2022/12/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PokemonSample/Shared/PokemonSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonSampleApp.swift
+//  Shared
+//
+//  Created by kensho hanano on 2022/12/09.
+//
+
+import SwiftUI
+
+@main
+struct PokemonSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
