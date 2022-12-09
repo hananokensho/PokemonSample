20c0cabb68a44f0c565c24d047b5e1edc82b561c
kensho hanano <>
2022-12-09 23:51:39 +0900

    ✨プロジェクト作成

---
+++ PokemonSample/PokemonSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonSample
+//
+//  Created by kensho hanano on 2022/12/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
